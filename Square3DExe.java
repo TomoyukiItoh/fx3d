70e1ab8b41038366f8e9cb0924ac9f33dd9c645c
TomoyukiItoh <to@totomo.net>
2019-07-29 09:59:34 +0900

    Create Square3DExe.java

---
+++ Square3DExe.java
@@ -0,0 +1,25 @@
+package fx3d;
+
+import tomojavalib.mesh.ThreeDimension;
+
+/**
+ * Square3Dに関する計算を行う
+ * */
+public class Square3DExe {
+
+/**コンストラクタ*/
+public Square3DExe() {}
+
+
+/**設定した3D空間内にある幅wのSquare3D配列を返す
+ * @param max 3D空間の最大位置
+ * @param min 3D空間の最小位置
+ * @param w Square3Dの幅
+ * @return
+ */
+public Square3D[] getSquare3DsInArea( ThreeDimension max , ThreeDimension min ,  double w ) {
+Square3D[] v = null;
+return v;
+}
+
+}
